4ad608a10d5eb84c6c7da04c3c3816495b22ab97
unknown <gustavoluizmiranda405@gmail.com>
2024-03-15 15:08:40 -0300

    form de criação de projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,15 @@
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css'
+
 function NewProject() {
-    return <h1>NewProject</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p>formulário</p>
+            <ProjectForm btnText="Criar projeto"/>
+        </div>
+    )
 }
 
 export default NewProject;
